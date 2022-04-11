20ecd0c2cf997271ca1b4b940f6775ddb9826b78
Nico Picchio <nicolapicchio@gmail.com>
2022-04-11 15:27:05 +0100

    exercise completed

--- src/router.js
+++ src/router.js
@@ -13,12 +13,20 @@ const mockUser = {
 	},
 };
 
+const secretKey = 'superSecret';
+
 router.post('/login', (req, res) => {
-	
+	res.json(jwt.sign({ username: mockUser.username }, secretKey));
 });
 
 router.get('/profile', (req, res) => {
-	res.json(mockUser);
+	try {
+		const token = req.headers.authorization.substring(7);
+		jwt.verify(token, secretKey);
+		res.status(200).json({ profile: mockUser.profile });
+	} catch (err) {
+		res.status(401).json('Who are you?');
+	}
 });
 
 module.exports = router;
